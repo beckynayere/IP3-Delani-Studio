09cfa2e3ca24e78527e0687163ee4b9d7c23bc7b
becky nayere <rebecca.nayere@gmail.com>
2020-07-26 17:08:17 +0300

    add the jquery

---
+++ js/scripts.js
@@ -0,0 +1,7 @@
+            // pop up the message with the name
+            if(name == "" || email == "" || message == "") {
+                document.getElementById("popUp").innerHTML = "Sorry! you did not enter a name or email or a message."
+                alert("Sorry! you did not enter a name or email or a message.")
+            }else{
+                document.getElementById("popUp").innerHTML = "*"  + name + myMessage ; // 
+                alert("*"  + name + myMessage )
