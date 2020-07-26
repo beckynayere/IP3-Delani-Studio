2c4d213c159c159ad6511ab0e557c7b0926e2da1
becky nayere <rebecca.nayere@gmail.com>
2020-07-27 01:48:42 +0300

    add README.md

--- js/scripts.js
+++ js/scripts.js
@@ -3,7 +3,7 @@
                 document.getElementById("popUp").innerHTML = "Sorry! you did not enter a name or email or a message."
                 alert("Sorry! you did not enter a name or email or a message.")
             }else{
-                document.getElementById("popUp").innerHTML = "*"  + name + myMessage ; // 
+                document.getElementById("popUp").innerHTML = "*"  + name + myMessage ; 
                 alert("*"  + name + myMessage );
                 
                 
